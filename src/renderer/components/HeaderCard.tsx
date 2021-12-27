4df521f233dd8f2fbced429efaa75662a13f59be
Kevin Lu <18564130+KevinLu@users.noreply.github.com>
2021-12-27 13:48:31 -0500

    dark mode for HeaderCard

--- src/renderer/components/HeaderCard.tsx
+++ src/renderer/components/HeaderCard.tsx
@@ -1,12 +1,14 @@
 import { HeaderCardProps } from 'main/typings';
-import { Box, Flex, Heading, Spacer, Text } from '@chakra-ui/react';
+import { Box, Flex, Text, useColorModeValue } from '@chakra-ui/react';
 
 const HeaderCard = ({ header, children }: HeaderCardProps) => {
+  const color = useColorModeValue('gray.600', 'gray.400');
+
   return (
     <Flex height="100%" width="100%" direction="column">
-      <Heading fontFamily="monospace" textTransform="uppercase" p={2} color="gray.600" size="xs">
+      <Text fontWeight="medium" pt="4" pl="4" color={color}>
         {header}
-      </Heading>
+      </Text>
 
       <Box flex="1">{children}</Box>
     </Flex>
