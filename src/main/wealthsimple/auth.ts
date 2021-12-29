04b4f4ec134942d5d4134a042db72a66774f280a
Franco <3619860+picknchew@users.noreply.github.com>
2021-12-29 02:30:36 -0500

    fix login and bump version

--- src/main/wealthsimple/auth.ts
+++ src/main/wealthsimple/auth.ts
@@ -52,7 +52,7 @@ const login = async (username: string, password: string, otp?: string) => {
   store.set('tokenType', res.data.token_type);
   store.set('accessToken', res.data.access_token);
   store.set('refreshToken', res.data.refresh_token);
-  store.set('userId', res.data.canonical_user_id);
+  store.set('userId', res.data.user_canonical_id);
 
   return LoginResponse.Success;
 };
