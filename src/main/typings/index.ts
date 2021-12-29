93820c96a9cd68cb4725b2776beb4ff942f0946b
Kevin Lu <kevinqianlu@gmail.com>
2021-12-29 13:13:41 -0500

    chore: bump app version to 1.1.1

--- src/main/typings/index.ts
+++ src/main/typings/index.ts
@@ -1,6 +1,6 @@
 import { QueryObserverResult, RefetchOptions, RefetchQueryFilters } from 'react-query';
 
-export const APP_VERSION = '1.0.0';
+export const APP_VERSION = '1.1.1';
 
 export interface AuthContextInterface {
   isAuth: boolean;
